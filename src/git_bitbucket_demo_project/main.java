3a5ae80de98a3806093e2067b097433bec203ccb
Shane <A00279073@student.ait.ie>
2020-08-12 00:54:38 +0100

    First commit to bitBucket

--- src/git_bitbucket_demo_project/main.java
+++ src/git_bitbucket_demo_project/main.java
@@ -3,7 +3,7 @@ package git_bitbucket_demo_project;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello World");
 
 	}
 
